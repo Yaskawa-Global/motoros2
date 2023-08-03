d8a9585ae45738b2f7548ab78af418094f51fc2b
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2023-08-03 11:48:04 +0200

    Use the updated Ros_ErrorHandling_ErrNo_ToString(..)

--- src/MotionControl.c
+++ src/MotionControl.c
@@ -1287,7 +1287,8 @@ BOOL Ros_MotionControl_StartMotionMode(MOTION_MODE mode)
     {
         //TODO(gavanderhoorn): special check for "job is not loaded"
         Ros_Debug_BroadcastMsg(
-            "Can't start '%s' because: 0x%04X %s", g_nodeConfigSettings.inform_job_name, rData.err_no, Ros_ErrorHandling_Only_ErrNo_ToString(rData.err_no));
+            "Can't start '%s' because: 0x%04X %s", g_nodeConfigSettings.inform_job_name, rData.err_no,
+            Ros_ErrorHandling_ErrNo_ToString(rData.err_no));
         goto updateStatus;
     }
 
