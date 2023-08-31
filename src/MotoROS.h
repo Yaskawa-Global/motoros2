d5f8d39839bba213c0a6e8866d786de2c9a715cc
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2023-08-31 14:37:17 +0200

    Bump version as 0.1.2pre
    Facilitate distinguishing from-source builds from binary releases.

--- src/MotoROS.h
+++ src/MotoROS.h
@@ -9,7 +9,7 @@
 #define MOTOROS2_MOTOROS_H
 
 #define APPLICATION_NAME            "MotoROS2"
-#define APPLICATION_VERSION         "0.1.1"
+#define APPLICATION_VERSION         "0.1.2pre"
 
 #include "motoPlus.h"
 
