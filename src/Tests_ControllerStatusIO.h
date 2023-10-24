42a31cb50f41f27a01e881a89e5e8d4c5b3ea310
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2023-10-24 14:46:32 +0200

    Add tests for ShouldWarnNoCalibDataLoaded(..)
    Initial set to encode assumptions.

---
+++ src/Tests_ControllerStatusIO.h
@@ -0,0 +1,17 @@
+// Tests_ControllerStatusIO.h
+
+// SPDX-FileCopyrightText: 2024, Yaskawa America, Inc.
+// SPDX-FileCopyrightText: 2024, Delft University of Technology
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MOTOROS2_TESTS_CONTROLLER_STATUS_IO_H
+#define MOTOROS2_TESTS_CONTROLLER_STATUS_IO_H
+
+#ifdef MOTOROS2_TESTING_ENABLE
+
+extern BOOL Ros_Testing_ControllerStatusIO();
+
+#endif //MOTOROS2_TESTING_ENABLE
+
+#endif  // MOTOROS2_TESTS_CONTROLLER_STATUS_IO_H
