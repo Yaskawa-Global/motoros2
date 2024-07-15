b308167a229c28859debd23e2dac27cb0857754f
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2024-07-15 15:18:36 +0200

    Implement 'inform_job/list' callback

--- src/ServiceInformJobList.c
+++ src/ServiceInformJobList.c
@@ -15,11 +15,15 @@ typedef motoros2_interfaces__srv__ListInformJobs_Response ListInformJobsResponse
 
 
 static const size_t RESP_NAMES_LIST_INITIAL_SIZE = 1;
+static const size_t MAX_NUMBER_OF_JOBS_TO_REPORT = 1024;
 
 
 void Ros_ServiceListInformJobs_Initialize()
 {
     MOTOROS2_MEM_TRACE_START(svc_list_jobs_init);
+
+    Ros_Debug_BroadcastMsg("%s: maximum nr of jobs supported: %d",
+        __func__, MAX_NUMBER_OF_JOBS_TO_REPORT);
 
     const rosidl_service_type_support_t* type_support =
         ROSIDL_GET_SRV_TYPE_SUPPORT(motoros2_interfaces, srv, ListInformJobs);
@@ -59,6 +63,108 @@ void Ros_ServiceListInformJobs_Trigger(const void* request_msg, void* response_m
     ListInformJobsResponse* response = (ListInformJobsResponse*)response_msg;
 
     Ros_Debug_BroadcastMsg("%s: enter", __func__);
+
+    //make sure we don't accidentally return old results (in case of errors fi)
+    rosidl_runtime_c__String__assign(&response->message, "");
+    response->result_code = 0;
+    response->names.size = 0;
+
+    //retrieve list of jobs (".JBI")
+    Ros_Debug_BroadcastMsg("%s: updating job file list", __func__);
+    LONG status = mpRefreshFileList(MP_EXT_ID_JBI);
+    if (status != OK)
+    {
+        Ros_Debug_BroadcastMsg("%s: error refreshing job list", __func__);
+        rosidl_runtime_c__String__assign(&response->message, "error refreshing job list");
+        //TODO(gavanderhoorn): define status/error codes
+        response->result_code = 10;
+        goto DONE;
+    }
+
+    //see how many jobs there are, and if necessary, resize list of names in service response
+    size_t job_count = mpGetFileCount();
+    if (job_count < 0)
+    {
+        Ros_Debug_BroadcastMsg("%s: error retrieving file count: %d", __func__, job_count);
+        rosidl_runtime_c__String__assign(&response->message, "error retrieving file count");
+        //TODO(gavanderhoorn): define status/error codes
+        response->result_code = 11;
+        goto DONE;
+    }
+    Ros_Debug_BroadcastMsg("%s: found %d jobs", __func__, job_count);
+
+    //there is a limit to the number of jobs we support
+    if (job_count > MAX_NUMBER_OF_JOBS_TO_REPORT)
+    {
+        Ros_Debug_BroadcastMsg("%s: too many jobs, aborting", __func__);
+        rosidl_runtime_c__String__assign(&response->message, "too many jobs");
+        //TODO(gavanderhoorn): define status/error codes
+        response->result_code = 12;
+        goto DONE;
+    }
+
+    //resize
+    if (job_count > response->names.capacity)
+    {
+        Ros_Debug_BroadcastMsg("%s: resize needed: current: %d; need: %d",
+            __func__, response->names.capacity, job_count);
+        rosidl_runtime_c__String__Sequence__fini(&response->names);
+        //TODO(gavanderhoorn): handle errors
+        rosidl_runtime_c__String__Sequence__init(&response->names, job_count);
+    }
+
+    //copy all jobs on the list to the response
+    //name, plus extension, plus terminating null
+#define JOB_EXT_LEN (4)
+#define MAX_JOB_NAME_LEN_WITH_EXT (MAX_JOB_NAME_LEN + JOB_EXT_LEN + 1)
+    char name_buf[MAX_JOB_NAME_LEN_WITH_EXT] = { 0 };
+    for (size_t i = 0; i < job_count; ++i)
+    {
+        //retrieve job name from internal list
+        bzero(name_buf, MAX_JOB_NAME_LEN_WITH_EXT);
+        status = mpGetFileName(i, name_buf);
+        if (status != OK)
+        {
+            Ros_Debug_BroadcastMsg("%s: failed retrieving job %d from list", __func__, i);
+            rosidl_runtime_c__String__assign(&response->message, "failed retrieving job from list");
+            //TODO(gavanderhoorn): define status/error codes
+            response->result_code = 13;
+            goto DONE;
+        }
+
+        //determine length without extension (which is 4 less than the full name)
+        size_t name_len = Ros_strnlen(name_buf, MAX_JOB_NAME_LEN_WITH_EXT);
+        if (name_len <= JOB_EXT_LEN)
+        {
+            Ros_Debug_BroadcastMsg("%s: job name '%s' too short (%d chars), can't remove extension",
+                __func__, name_buf, name_len);
+        }
+        else
+        {
+            name_len -= JOB_EXT_LEN;
+        }
+
+        //add it to the response
+        if (!rosidl_runtime_c__String__assignn(&response->names.data[i], name_buf, name_len))
+        {
+            Ros_Debug_BroadcastMsg("%s: failed adding job name %d to response, aborting", __func__, i);
+            rosidl_runtime_c__String__assign(&response->message, "Error adding job name to response");
+            //TODO(gavanderhoorn): define status/error codes
+            response->result_code = 14;
+            goto DONE;
+        }
+    }
+#undef JOB_EXT_LEN
+#undef MAX_JOB_NAME_LEN_WITH_EXT
+
+    //finally update nr of items in list
+    response->names.size = job_count;
+
+    //done, report to client
+    Ros_Debug_BroadcastMsg("%s: returning %d job names", __func__, response->names.size);
+    rosidl_runtime_c__String__assign(&response->message, "success");
+    //TODO(gavanderhoorn): define status/error codes
+    response->result_code = 1;
 
 DONE:
     Ros_Debug_BroadcastMsg("%s: exit", __func__);
