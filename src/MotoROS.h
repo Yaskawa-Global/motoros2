8aef0a43e57067d811140661cccea41b9df5fc8d
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2024-07-15 15:17:11 +0200

    Add skeleton 'inform_job/list' service
    As implied by the name of the service, this allows clients to retrieve a list of INFORM jobs (JBI) on the controller.

--- src/MotoROS.h
+++ src/MotoROS.h
@@ -78,6 +78,7 @@
 #include <motoros2_interfaces/srv/select_motion_tool.h>
 #include <builtin_interfaces/msg/duration.h>
 #include <builtin_interfaces/msg/time.h>
+#include <motoros2_interfaces/srv/list_inform_jobs.h>
 
 //============================================
 // MotoROS
@@ -102,6 +103,7 @@
 #include "ServiceStartPointQueueMode.h"
 #include "ServiceStopTrajMode.h"
 #include "ServiceSelectMotionTool.h"
+#include "ServiceInformJobList.h"
 #include "MotionControl.h"
 #include "ConfigFile.h"
 #include "RosApiNameConstants.h"
