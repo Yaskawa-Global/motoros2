ae2b9aa8d1fe80f388b8dcd969d3622ff58d095a
jimmy-mcelwain <jimmy.mcelwain@motoman.com>
2024-08-30 16:02:58 -0400

    Initial framework for reporting rcl errors

--- src/ErrorHandling.c
+++ src/ErrorHandling.c
@@ -76,6 +76,164 @@ const char* const Ros_ErrorHandling_MotionNotReadyCode_ToString(MotionNotReadyCo
     }
 }
 
+void Ros_ErrorHandling_Container_rcl_rclc_fail_info_Populate(int errNo,Container_rcl_rclc_fail_info* info) {
+    switch (errNo)
+    {
+    case RCL_RET_OK:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - Success");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_OK;
+        break;
+    case RCL_RET_ERROR:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - Unspecified error");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_ERROR;
+        break;
+    case RCL_RET_TIMEOUT:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - Timeout occurred");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_TIMEOUT;
+        break;
+    case RCL_RET_BAD_ALLOC:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC failed to alloc memory");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_BAD_ALLOC;
+        break;
+    case RCL_RET_INVALID_ARGUMENT:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - Invalid argument");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_INVALID_ARGUMENT;
+        break;
+    case RCL_RET_UNSUPPORTED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC- unsupported ret. code");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_UNSUPPORTED;
+        break;
+    case RCL_RET_ALREADY_INIT:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_init() already called");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_ALREADY_INIT;
+        break;
+    case RCL_RET_NOT_INIT:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_init() not yet called");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_NOT_INIT;
+        break;
+    case RCL_RET_MISMATCHED_RMW_ID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC mismatched rmw ident.");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_MISMATCHED_RMW_ID;
+        break;
+    case RCL_RET_TOPIC_NAME_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - topic name invalid");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_TOPIC_NAME_INVALID;
+        break;
+    case RCL_RET_SERVICE_NAME_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - service name invalid");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_SERVICE_NAME_INVALID;
+        break;
+    case RCL_RET_UNKNOWN_SUBSTITUTION:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC topic name sub unknown");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_UNKNOWN_SUBSTITUTION;
+        break;
+    case RCL_RET_ALREADY_SHUTDOWN:
+        rosidl_runtime_c__String__assign(&info->message, "rclc_shutdown() already called");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_ALREADY_SHUTDOWN;
+        break;
+    case RCL_RET_NODE_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "Invalid rcl_node_t given");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_NODE_INVALID;
+        break;
+    case RCL_RET_NODE_INVALID_NAME:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC invalid node name");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_NODE_INVALID_NAME;
+        break;
+    case RCL_RET_NODE_INVALID_NAMESPACE:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC invalid node namespace");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_NODE_INVALID_NAMESPACE;
+        break;
+    case RCL_RET_NODE_NAME_NON_EXISTENT:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC cannot find node name");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_NODE_NAME_NON_EXISTENT;
+        break;
+    case RCL_RET_PUBLISHER_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "Invalid rcl_publisher_t given");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_PUBLISHER_INVALID;
+        break;
+    case RCL_RET_SUBSCRIPTION_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "Invalid rcl_subscription_t");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_SUBSCRIPTION_INVALID;
+        break;
+    case RCL_RET_SUBSCRIPTION_TAKE_FAILED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC Take msg from sub fail");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_SUBSCRIPTION_TAKE_FAILED;
+        break;
+    case RCL_RET_CLIENT_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "Invalid rcl_client_t given");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_CLIENT_INVALID;
+        break;
+    case RCL_RET_CLIENT_TAKE_FAILED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC client take res failed");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_CLIENT_TAKE_FAILED;
+        break;
+    case RCL_RET_SERVICE_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC invalid rcl_service_t");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_SERVICE_INVALID;
+        break;
+    case RCL_RET_SERVICE_TAKE_FAILED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCL get service req. failed");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_SERVICE_TAKE_FAILED;
+        break;
+    case RCL_RET_TIMER_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - invalid rcl_timer_t ");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_TIMER_INVALID;
+        break;
+    case RCL_RET_TIMER_CANCELED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - given timer canceled");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_TIMER_CANCELED;
+        break;
+    case RCL_RET_WAIT_SET_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "Invalid rcl_wait_set_t given");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_WAIT_SET_INVALID;
+        break;
+    case RCL_RET_WAIT_SET_EMPTY:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_wait_set_t is empty");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_WAIT_SET_EMPTY;
+        break;
+    case RCL_RET_WAIT_SET_FULL:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_wait_set_t is full");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_WAIT_SET_FULL;
+        break;
+    case RCL_RET_INVALID_REMAP_RULE:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC not a valid remap rule");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_INVALID_REMAP_RULE;
+        break;
+    case RCL_RET_WRONG_LEXEME:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC wrong lexeme");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_WRONG_LEXEME;
+        break;
+    case RCL_RET_INVALID_ROS_ARGS:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC found invalid ros arg");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_INVALID_ROS_ARGS;
+        break;
+    case RCL_RET_INVALID_PARAM_RULE:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC Arg not valid param");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_INVALID_PARAM_RULE;
+        break;
+    case RCL_RET_INVALID_LOG_LEVEL_RULE:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC Arg not valid log lvl");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_INVALID_LOG_LEVEL_RULE;
+        break;
+    case RCL_RET_EVENT_INVALID:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC - Invalid rcl_event_t");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_EVENT_INVALID;
+        break;
+    case RCL_RET_EVENT_TAKE_FAILED:
+        rosidl_runtime_c__String__assign(&info->message, "RCL/RCLC Failed to get event");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_EVENT_TAKE_FAILED;
+        break;
+    case RCL_RET_LIFECYCLE_STATE_REGISTERED:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_lifecycle state registered");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_LIFECYCLE_STATE_REGISTERED;
+        break;
+    case RCL_RET_LIFECYCLE_STATE_NOT_REGISTERED:
+        rosidl_runtime_c__String__assign(&info->message, "rcl_lifecycle state not reg.");
+        info->subcode = SUBCODE_RCL_RCLC_FAIL_LIFECYCLE_STATE_NOT_REGISTERED;
+        break;
+    }
+}
+
 void motoRosAssert(BOOL mustBeTrue, ALARM_ASSERTION_FAIL_SUBCODE subCodeIfFalse)
 {
     motoRosAssert_withMsg(mustBeTrue, subCodeIfFalse, APPLICATION_NAME ": Fatal Error");
@@ -106,6 +264,65 @@ void motoRosAssert_withMsg(BOOL mustBeTrue, ALARM_ASSERTION_FAIL_SUBCODE subCode
     FOREVER
     {
         Ros_Debug_BroadcastMsg("motoRosAssert: %s (subcode: %d)", msg, subCodeIfFalse);
+        Ros_Sleep(5000);
+    }
+}
+
+void motoRosAssert_RCLAssertOK(rcl_ret_t code)
+{
+    if (code == RCL_RET_OK)
+        return;
+
+    Container_rcl_rclc_fail_info info;
+
+    Ros_ErrorHandling_Container_rcl_rclc_fail_info_Populate(code, &info);
+
+    Ros_Controller_SetIOState(IO_FEEDBACK_FAILURE, TRUE);
+    Ros_Controller_SetIOState(IO_FEEDBACK_INITIALIZATION_DONE, FALSE);
+
+    mpSetAlarm(ALARM_RCL_RCLC_FAIL, info.message.data, info.subcode);
+
+    FOREVER
+    {
+        Ros_Debug_BroadcastMsg("motoRos_Rcl_Rclc_AssertOK: %s (subcode: %d)", info.message.data, info.subcode);
+        Ros_Sleep(5000);
+    }
+}
+
+void motoRosAssert_RCLAssertOK_withAlarmAssertionFailSubcode(rcl_ret_t code, ALARM_ASSERTION_FAIL_SUBCODE subCodeIfFalse)
+{
+    motoRosAssert_RCLAssertOK_withAlarmAssertionFailSubcode_withMsg(code, subCodeIfFalse, APPLICATION_NAME ": Fatal Error");
+}
+
+void motoRosAssert_RCLAssertOK_withAlarmAssertionFailSubcode_withMsg(rcl_ret_t code, ALARM_ASSERTION_FAIL_SUBCODE subCodeIfFalse, char* msgFmtIfFalse, ...)
+{
+    const int MAX_MSG_LEN = 32;
+    char msg[MAX_MSG_LEN];
+    va_list va;
+    Container_rcl_rclc_fail_info rcl_ret_info;
+
+    if (code == RCL_RET_OK)
+        return;
+
+    bzero(msg, MAX_MSG_LEN);
+
+    va_start(va, msgFmtIfFalse);
+    vsnprintf(msg, MAX_MSG_LEN, msgFmtIfFalse, va);
+    va_end(va);
+
+    Ros_ErrorHandling_Container_rcl_rclc_fail_info_Populate(code, &rcl_ret_info);
+
+    Ros_Controller_SetIOState(IO_FEEDBACK_FAILURE, TRUE);
+    Ros_Controller_SetIOState(IO_FEEDBACK_INITIALIZATION_DONE, FALSE);
+
+    mpSetAlarm(ALARM_RCL_RCLC_FAIL, rcl_ret_info.message.data, rcl_ret_info.subcode);
+    mpSetAlarm(ALARM_ASSERTION_FAIL, msg, subCodeIfFalse);
+
+
+    FOREVER
+    {
+        Ros_Debug_BroadcastMsg("motoRosAssert_RCLAssertOK_withAlarmAssertionFailSubcode_withMsg: %s (subcode: %d)", msg, subCodeIfFalse);
+        Ros_Debug_BroadcastMsg("motoRosAssert_RCLAssertOK_withAlarmAssertionFailSubcode_withMsg: %s (subcode: %d)", rcl_ret_info.message.data, rcl_ret_info.subcode);
         Ros_Sleep(5000);
     }
 }
