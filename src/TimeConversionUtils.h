2a1fd36dfdeefba119c12a8af52a33f4b9b47d15
jimmy-mcelwain <jimmy.mcelwain@motoman.com>
2025-01-22 12:59:54 -0500

    Add more time conversion functions

--- src/TimeConversionUtils.h
+++ src/TimeConversionUtils.h
@@ -34,6 +34,23 @@ static inline void Ros_Nanos_To_Duration_Msg(INT64 x, builtin_interfaces__msg__D
     y->nanosec = (x % 1000000000LL);
 }
 
+static inline INT64 Ros_Time_Msg_To_Millis(builtin_interfaces__msg__Time const* const x)
+{
+    return ((INT64)x->sec * 1000) + (INT64)(x->nanosec * 0.000001);
+}
+
+static inline INT64 Ros_Time_Msg_To_Nanos(builtin_interfaces__msg__Time const* const x)
+{
+    return ((INT64)x->sec * 1000000000LL) + (INT64)x->nanosec;
+}
+
+static inline void Ros_Millis_To_Time_Msg(INT64 x, builtin_interfaces__msg__Time* const y)
+{
+    y->sec = x / 1000;
+    y->nanosec = (x % 1000) * 1000000;
+}
+
+
 static inline void Ros_Nanos_To_Time_Msg(INT64 x, builtin_interfaces__msg__Time* const y)
 {
     y->sec = x / 1000000000LL;
