371c902ef0fe84c5ecdae992938e8c81a42d9e6e
jimmy-mcelwain <jimmy.mcelwain@motoman.com>
2025-04-08 17:22:07 -0400

    Enable ControllerStatusIO tests

--- src/Tests_ControllerStatusIO.c
+++ src/Tests_ControllerStatusIO.c
@@ -9,8 +9,6 @@
 
 #include "MotoROS.h"
 
-#if 0
-
 void Ros_Testing_ControllerStatusIO_MakeFake6dofRobot(CtrlGroup* group, int groupNo, MP_GRP_ID_TYPE groupId)
 {
     bzero(group, sizeof(CtrlGroup));
@@ -344,14 +342,10 @@ BOOL Ros_Testing_ControllerStatusIO_ShouldWarnNoCalibDataLoaded_R1B1R2B2()
     return bSuccess;
 }
 
-#endif
 
 BOOL Ros_Testing_ControllerStatusIO()
 {
     BOOL bSuccess = TRUE;
-
-    // disabled for now, cause controllers to crash (stack overflow issue perhaps?)
-#if 0
 
     bSuccess &= Ros_Testing_ControllerStatusIO_ShouldWarnNoCalibDataLoaded_R1();
     bSuccess &= Ros_Testing_ControllerStatusIO_ShouldWarnNoCalibDataLoaded_R1B1();
@@ -362,7 +356,6 @@ BOOL Ros_Testing_ControllerStatusIO()
     bSuccess &= Ros_Testing_ControllerStatusIO_ShouldWarnNoCalibDataLoaded_R1B1S1();
     bSuccess &= Ros_Testing_ControllerStatusIO_ShouldWarnNoCalibDataLoaded_R1B1R2B2();
 
-#endif
 
     return bSuccess;
 }
