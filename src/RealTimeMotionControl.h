6d70c33bf1ae5e0c7abab7ab34b005d49274a0e3
Ted Miller <ted.miller@motoman.com>
2025-08-12 08:36:58 -0400

    RealTimeMotionControl header

---
+++ src/RealTimeMotionControl.h
@@ -0,0 +1,13 @@
+//RealTimeMotionControl.h
+
+// SPDX-FileCopyrightText: 2025, Yaskawa America, Inc.
+// SPDX-FileCopyrightText: 2025, Delft University of Technology
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MOTOROS2_REALTIME_MOTION_CONTROL_H
+#define MOTOROS2_REALTIME_MOTION_CONTROL_H
+
+extern void MotionControl_RtIncMoveLoopStart();
+
+#endif //MOTOROS2_REALTIME_MOTION_CONTROL_H
